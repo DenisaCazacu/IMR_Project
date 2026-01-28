aa2a017ef838475d642cb42a06b7def132cc34b4
DenisaCazacu <denisa.cazacu01@gmail.com>
2026-01-28 22:17:25 +0200

    Add audio player when you pass by a picture

---
+++ Assets/ProximityAudioTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ProximityAudioTrigger : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    private bool isActive = false;
+
+    public void EnableTrigger()
+    {
+        isActive = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isActive) return;
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player passed near the object => playing audio");
+            audioSource.Play();
+            isActive = false; // Only trigger once
+        }
+    }
+}
---
+++ Assets/TaskListener.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class TaskListener : MonoBehaviour
+{
+    [SerializeField] private string targetTaskId = "pick_phone";
+    [SerializeField] private ProximityAudioTrigger proximityTrigger;
+
+    private void OnEnable()
+    {
+        if (TasksTracker.Instance != null)
+        {
+            TasksTracker.Instance.OnTaskStarted += HandleTaskStarted;
+        }
+        else
+        {
+            Debug.LogWarning("TaskListener: TasksTracker.Instance is NULL in OnEnable. Will retry in Start.");
+        }
+    }
+
+    private void Start()
+    {
+        if (TasksTracker.Instance != null)
+        {
+            TasksTracker.Instance.OnTaskStarted += HandleTaskStarted;
+        }
+    }
+
+    private void OnDisable()
+    {
+        TasksTracker.Instance.OnTaskStarted -= HandleTaskStarted;
+    }
+
+    private void HandleTaskStarted(string taskId)
+    {
+        if (taskId == targetTaskId)
+        {
+            Debug.Log("PickPhone task started => enabling proximity trigger");
+            proximityTrigger.EnableTrigger();
+        }
+    }
+}
